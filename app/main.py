bdf052432a7be257150b0d6bb1e56c4c09bab56a
Zaaris1 <juangabrielmarket@gmail.com>
2026-01-09 12:49:21 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 from app.seed import seed_data
 from app.routers import auth, stores, tickets, admin
+from app.routers import networks  # ✅ novo
 
 app = FastAPI(title="RioAutocom Tech API", version="1.0.0-final")
 
@@ -21,5 +22,6 @@ seed_data()
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(admin.router, prefix="/admin", tags=["Admin"])
+app.include_router(networks.router, prefix="/networks", tags=["Networks"])  # ✅ novo
 app.include_router(stores.router, prefix="/stores", tags=["Stores"])
 app.include_router(tickets.router, prefix="/tickets", tags=["Tickets"])
